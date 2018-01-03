be5038940e23b36e77b661e3625941aba3f38570
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2018-01-03 16:09:40 +0100

    Add vimrc

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 
 # Backup current files
+## vim
+cp ~/.vimrc ~/.vimrc.backup
+## shell
 cp ~/.shell_aliases ~/.shell_aliases.backup
 cp ~/.shell_config  ~/.shell_config.backup
 cp ~/.shell_exports  ~/.shell_exports.backup
@@ -8,6 +11,9 @@ cp ~/.shell_functions  ~/.shell_functions.backup
 cp ~/.zshrc ~/.zshrc.backup
 
 # Copy new files
+## vim
+cp vim/vimrc ~/.vimrc
+## shell
 cp zsh/shell_aliases ~/.shell_aliases
 cp zsh/shell_config ~/.shell_config 
 cp zsh/shell_exports ~/.shell_exports 
