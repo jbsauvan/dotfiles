2dfe4c858f2931cef1243ed67633806b8d767e79
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2018-01-05 15:42:32 +0100

    Use backup and update flags in cp

--- install.sh
+++ install.sh
@@ -1,26 +1,15 @@
 #! /usr/bin/env bash
 
-# Backup current files
-## vim
-cp ~/.vimrc ~/.vimrc.backup
-## shell
-cp ~/.shell_aliases ~/.shell_aliases.backup
-cp ~/.shell_config  ~/.shell_config.backup
-cp ~/.shell_exports  ~/.shell_exports.backup
-cp ~/.shell_functions  ~/.shell_functions.backup
-cp ~/.zshrc ~/.zshrc.backup
-## git
-cp ~/.gitignore ~/.gitignore.backup
-
 # Copy new files
+# Do backup of old files and check if update needed
 ## vim
-cp vim/vimrc ~/.vimrc
+cp -b -u vim/vimrc ~/.vimrc
 ## shell
-cp zsh/shell_aliases ~/.shell_aliases
-cp zsh/shell_config ~/.shell_config 
-cp zsh/shell_exports ~/.shell_exports 
-cp zsh/shell_functions ~/.shell_functions 
-cp zsh/zshrc ~/.zshrc
+cp -b -u zsh/shell_aliases ~/.shell_aliases
+cp -b -u zsh/shell_config ~/.shell_config 
+cp -b -u zsh/shell_exports ~/.shell_exports 
+cp -b -u zsh/shell_functions ~/.shell_functions 
+cp -b -u zsh/zshrc ~/.zshrc
 ## git
-cp git/gitignore ~/.gitignore
+cp -b -u git/gitignore ~/.gitignore
 
